a1ef3c6593e6ca1d31c139b12b16e2390861dca1
Tathya <coding.tathya@gmail.com>
2026-04-23 01:28:06 +0530

    fix: Fixed event preview

--- src/lib/components/EventPreview.svelte
+++ src/lib/components/EventPreview.svelte
@@ -7,17 +7,19 @@
 
   const start_date = new Date(data.start);
   const end_date = data.end ? new Date(data.end) : undefined;
+
+  console.log(data);
 </script>
 
 <Button align_top={true}>
   <button class="w-full h-full flex flex-col gap-4 text-left" disabled>
     <img
       src={data.imgUrl}
-      alt={data.title}
+      alt={data.name}
       class="w-full aspect-[5/4] object-cover rounded-lg content-center"
     />
     <div class="flex flex-col gap-4">
-      <h2 class="text-2xl">{data.title}</h2>
+      <h2 class="text-2xl">{data.name}</h2>
       <div class="flex flex-col gap-2">
         <div class="flex gap-2 items-center text-sm text-primary/50">
           <Icon icon="mdi:calendar" class="w-5 h-5" />
